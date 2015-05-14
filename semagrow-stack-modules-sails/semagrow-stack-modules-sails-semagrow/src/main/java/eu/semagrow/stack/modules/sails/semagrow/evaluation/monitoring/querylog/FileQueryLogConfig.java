4bac68f74c45573696593c52efb909f5be32ea5e
Angelos Charalambidis <agcharal@gmail.com>
2015-05-14 17:12:30 +0300

    QueryLogConfig interface

---
+++ semagrow-stack-modules-sails/semagrow-stack-modules-sails-semagrow/src/main/java/eu/semagrow/stack/modules/sails/semagrow/evaluation/monitoring/querylog/FileQueryLogConfig.java
@@ -0,0 +1,19 @@
+package eu.semagrow.stack.modules.sails.semagrow.evaluation.monitoring.querylog;
+
+/**
+ * Created by angel on 14/5/2015.
+ */
+public class FileQueryLogConfig implements QueryLogConfig {
+
+    private String filename;
+
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
+    }
+
+}
---
+++ semagrow-stack-modules-sails/semagrow-stack-modules-sails-semagrow/src/main/java/eu/semagrow/stack/modules/sails/semagrow/evaluation/monitoring/querylog/QueryLogConfig.java
@@ -0,0 +1,8 @@
+package eu.semagrow.stack.modules.sails.semagrow.evaluation.monitoring.querylog;
+
+/**
+ * Created by angel on 14/5/2015.
+ */
+public interface QueryLogConfig {
+
+}
